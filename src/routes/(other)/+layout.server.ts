7730950455bbcd51c56af7ccfe2e1c1995c6dc20
Aryan Nagpal <arnnagpal@outlook.com>
2024-05-21 17:36:13 -0700

    merge!

--- src/routes/(other)/+layout.server.ts
+++
@@ -1,9 +0,0 @@
-import type { LayoutServerLoad } from './$types';
-import {redirect} from "@sveltejs/kit";
-
-export const load: LayoutServerLoad = (event) => {
-    if (event.locals.user) redirect(302, "/app");
-
-    return {
-    };
-};
